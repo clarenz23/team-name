9e4a579cfe3516fa5a435c54ba350cd40d897c36
lupin <2211689@slu.edu.ph>
2023-03-31 18:45:11 +0800

    Final Mid Act02, Check tasks

--- src/FinalTeamJMCGMidAct02.java
+++ src/FinalTeamJMCGMidAct02.java
@@ -1,1 +1,83 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class FinalTeamJMCGMidAct02 {
+    public static void main(String[] args) {
+        String filePath = "res/data.csv";
+        int[] arrivalTimes = new int[11];
+        int[] interarrivalTimes = new int[11];
+        int[] serviceTimes = new int[11];
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int index = 0;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                arrivalTimes[index] = Integer.parseInt(values[1]);
+                interarrivalTimes[index] = Integer.parseInt(values[2]);
+                serviceTimes[index] = Integer.parseInt(values[3]);
+                index++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Get simulation time from user
+        int simulationTime = 0; // TODO: get user input
+
+        // Initialize statistical accumulators
+        int partsProduced = 0;
+        int totalQueueWaitTime = 0;
+        int partsPassedQueue = 0;
+        int longestQueueTime = 0;
+        int totalSystemTime = 0;
+        int longestSystemTime = 0;
+        int queueLengthArea = 0;
+        int highestQueueLength = 0;
+        int serverBusyArea = 0;
+
+        // Initialize simulation variables
+        int clock = 0;
+        int[] nextArrivalTime = new int[11];
+        for (int i = 0; i < 11; i++) {
+            nextArrivalTime[i] = arrivalTimes[i];
+        }
+        int nextDepartureTime = Integer.MAX_VALUE;
+        int numInQueue = 0;
+
+        // Run simulation loop
+        while (clock < simulationTime) {
+            // Determine next event (arrival or departure)
+            int nextEventTime = Math.min(nextDepartureTime, getNextArrivalTime(nextArrivalTime));
+            int event = (nextEventTime == nextDepartureTime) ? 2 : 1;
+
+            // @Enrico Castro
+            // Update statistical accumulators
+            // TODO: update statistical accumulators
+
+            // @Joanne Mangsat
+            // Perform event
+            if (event == 1) { // arrival event
+                // TODO: handle arrival event
+            } else { // departure event
+                // TODO: handle departure event
+            }
+        }
+
+        // @Danielle Javier
+        // Print simulation results
+        // TODO: print simulation results
+    }
+
+    private static int getNextArrivalTime(int[] nextArrivalTime) {
+        int minTime = Integer.MAX_VALUE;
+        for (int i = 0; i < 11; i++) {
+            if (nextArrivalTime[i] < minTime) {
+                minTime = nextArrivalTime[i];
+            }
+        }
+        return minTime;
+    }
+}
