174330d8481b765fbc58930c94561b050fe77577
lupin <2211689@slu.edu.ph>
2023-04-10 09:56:03 +0800

    debugging

--- src/FinalTeamJMCGMidAct02.java
+++ src/FinalTeamJMCGMidAct02.java
@@ -13,13 +13,20 @@ public class FinalTeamJMCGMidAct02 {
         int[] serviceTimes = new int[11];
 
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            // Skip header row
+            String header = br.readLine();
             String line;
             int index = 0;
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
-                arrivalTimes[index] = Integer.parseInt(values[1]);
-                interarrivalTimes[index] = Integer.parseInt(values[2]);
-                serviceTimes[index] = Integer.parseInt(values[3]);
+                try {
+                    arrivalTimes[index] = (int) Double.parseDouble(values[1]);
+                    interarrivalTimes[index] = (int) Double.parseDouble(values[2]);
+                    serviceTimes[index] = (int) Double.parseDouble(values[3]);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                    return;
+                }
                 index++;
             }
         } catch (IOException e) {
@@ -28,8 +35,17 @@ public class FinalTeamJMCGMidAct02 {
         }
 
         // Get simulation time from user
-        System.out.print("Enter the simulation time (in minutes): ");
-        double simulationTime = sc.nextDouble();
+        double simulationTime;
+        while (true) {
+            try {
+                System.out.print("Enter the simulation time (in minutes): ");
+                simulationTime = sc.nextDouble();
+                break;
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a number.");
+                sc.nextLine(); // consume the invalid input
+            }
+        }
 
         // Initialize statistical accumulators
         int partsProduced = 0;
@@ -59,61 +75,89 @@ public class FinalTeamJMCGMidAct02 {
 
             int queueLength = numInQueue;
             totalSystemTime += queueLength + (nextDepartureTime - clock);
-            longestSystemTime = Math.max(longestSystemTime, queueLength + (nextDepartureTime - clock));
+            longestSystemTime = Math.max(longestSystemTime, queueLength + (nextEventTime - clock));
             queueLengthArea += queueLength * (nextEventTime - clock);
             highestQueueLength = Math.max(highestQueueLength, queueLength);
-            serverBusyArea += (event == 2) ? (nextEventTime - clock) : 0;
+            serverBusyArea += (nextEventTime - clock) * (11 - numInQueue);
+
+            // Update clock
+            clock = nextEventTime;
+
             if (event == 1 && queueLength == 0) {
                 totalQueueWaitTime += 0;
             } else if (event == 1) {
-                totalQueueWaitTime += (nextDepartureTime - clock) - serviceTimes[11 - numInQueue];
+                totalQueueWaitTime += (clock - nextArrivalTime[getNextArrivalTime(nextArrivalTime)]);
             } else if (event == 2) {
                 partsProduced++;
             }
 
-            // Perform event
-            if (event == 1) { // arrival event
-                // TODO: handle arrival event
-                numInQueue++;
-                if (numInQueue == 1) {
-                    nextDepartureTime = clock + serviceTimes[11 - numInQueue];
-                }
-                int index = getNextArrivalTime(nextArrivalTime);
-                nextArrivalTime[index] = clock + interarrivalTimes[index];
-            } else { // departure event
-                // TODO: handle departure event
-                numInQueue--;
+            if (event == 1) { // Arrival event
+                // Update statistical accumulators
+                partsProduced++;
                 if (numInQueue > 0) {
-                    nextDepartureTime = clock + serviceTimes[11 - numInQueue];
+                    totalQueueWaitTime += (clock - nextArrivalTime[0]);
+                    partsPassedQueue++;
+                    longestQueueTime = Math.max(longestQueueTime, (clock - nextArrivalTime[0]));
+                    // Shift all remaining arrival times forward
+                    for (int i = 1; i <= numInQueue; i++) {
+                        nextArrivalTime[i - 1] = nextArrivalTime[i];
+                    }
+                    numInQueue--;
+                }
+                // Schedule next arrival
+                int interarrivalTime = interarrivalTimes[partsProduced % 11];
+                nextArrivalTime[numInQueue] = clock + interarrivalTime;
+                numInQueue++;
+
+            } else { // Departure event
+                // Update statistical accumulators
+                if (numInQueue > 0) {
+                    totalQueueWaitTime += (clock - nextArrivalTime[0]);
+                    partsPassedQueue++;
+                    longestQueueTime = Math.max(longestQueueTime, (clock - nextArrivalTime[0]));
+                    // Shift all remaining arrival times forward
+                    for (int i = 1; i <= numInQueue; i++) {
+                        nextArrivalTime[i - 1] = nextArrivalTime[i];
+                    }
+                    numInQueue--;
+                    // Schedule next departure
+                    int serviceTime = serviceTimes[partsProduced % 11];
+                    nextDepartureTime = clock + serviceTime;
+                    serverBusyArea += serviceTime;
                 } else {
+                    // No parts waiting in queue, server becomes idle
                     nextDepartureTime = Integer.MAX_VALUE;
                 }
             }
         }
-            }
+
+        // Compute and print simulation results
+        double avgQueueWaitTime = (double) totalQueueWaitTime / partsPassedQueue;
+        double avgSystemTime = (double) totalSystemTime / partsProduced;
+        double avgQueueLength = (double) queueLengthArea / simulationTime;
+        double avgServerUtilization = (double) serverBusyArea / simulationTime;
+        System.out.println("Simulation Results:");
+        System.out.printf("Total Parts Produced: %d\n", partsProduced);
+        System.out.printf("Average Queue Wait Time: %.2f minutes\n", avgQueueWaitTime);
+        System.out.printf("Longest Queue Time: %d minutes\n", longestQueueTime);
+        System.out.printf("Average System Time: %.2f minutes\n", avgSystemTime);
+        System.out.printf("Longest System Time: %d minutes\n", longestSystemTime);
+        System.out.printf("Average Queue Length: %.2f\n", avgQueueLength);
+        System.out.printf("Highest Queue Length: %d\n", highestQueueLength);
+        System.out.printf("Average Server Utilization: %.2f%%\n", avgServerUtilization * 100);
+    }
 
     private static int getNextArrivalTime(int[] nextArrivalTime) {
         int minTime = Integer.MAX_VALUE;
+        int minIndex = 0;
         for (int i = 0; i < 11; i++) {
             if (nextArrivalTime[i] < minTime) {
                 minTime = nextArrivalTime[i];
+                minIndex = i;
             }
         }
-        return minTime;
+        return minIndex;
     }
-
-    // Print simulation results
-    System.out.println("Simulation Results:");
-    System.out.println("Simulation Time (in minutes): " + simulationTime);
-    System.out.println("Total Parts Produced: " + partsProduced);
-    System.out.println("Total Queue Wait Time: " + totalQueueWaitTime);
-    System.out.println("Total Parts Passed Queue: " + partsPassedQueue);
-    System.out.println("Longest Queue Time: " + longestQueueTime);
-    System.out.println("Total System Time: " + totalSystemTime);
-    System.out.println("Longest System Time: " + longestSystemTime);
-    System.out.println("Queue Length Area: " + queueLengthArea);
-    System.out.println("Highest Queue Length: " + highestQueueLength);
-    System.out.println("Server Busy Area: " + serverBusyArea);
 }
 
 
