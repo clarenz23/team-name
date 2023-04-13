357651b3b381d90e810de918d53c69eff0024ccd
lupin <2211689@slu.edu.ph>
2023-04-13 16:54:19 +0800

    fix pero may error

--- src/RedoMidAct02.java
+++ src/RedoMidAct02.java
@@ -21,20 +21,21 @@ public class RedoMidAct02 {
     private static double areaUnderServerBusy;
     private static int[] arrivalTimes = new int[11];
     private static int[] interarrivalTimes = new int[11];
-    private static int[] serviceTimes = new int[11];
+    private static double[] serviceTimes = new double[11];
+
 
     private static void statisticalAccumulators() {
-        System.out.printf("%-4d %-8.2f %-4d %-4d %-8.2f %-4d %-8.2f %-4d %-8.2f%n", numPartsProduced, totalWaitingTime, numPartsProcessed,
+        System.out.printf("%-4d %-4f %-4d %-4d %-4f %-4d %-4f %-4d %-4f%n", numPartsProduced, totalWaitingTime, numPartsProcessed,
                 maxTimeInQueue, totalSystemTime, maxTimeInSystem, areaUnderQLength, highestQLength, areaUnderServerBusy);
     }
 
     private static void attributes() {
-        System.out.printf("%-24s %-28s %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d%n",
+        System.out.printf("%-24s %-29s %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4f %-4d %-4f%n",
                 "", "", numPartsInQueue, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, areaUnderQLength, numPartsInQueue, areaUnderServerBusy);
     }
 
     private static void variables() {
-        System.out.printf("%-10s %-6.2f %-14s %-6d %-8d %-24s %-28s %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d %-4d%n",
+        System.out.printf("%-10s %-6.1f %-14s %-6d %-8d %-24s %-28s %-4d %-4d %-4d %-4d %-4d %-4d %-4f %-4d %-4f%n",
                 "", currentTime, "", numPartsInQueue, 0, "", "", 0, 0, 0, 0, 0, 0, 0, areaUnderQLength, numPartsInQueue, areaUnderServerBusy);
     }
 
@@ -47,9 +48,9 @@ public class RedoMidAct02 {
         totalWaitingTime += Math.max(0, currentTime - arrivalTimes[numPartsProduced - 1]);
         numPartsInQueue--;
         numPartsProcessed++;
-        totalSystemTime += serviceTimes[numPartsProduced];
+        totalSystemTime += serviceTimes[numPartsProduced - 1];
         maxTimeInQueue = Math.max(maxTimeInQueue, (int) (currentTime - arrivalTimes[numPartsProduced - 1]));
-        maxTimeInSystem = Math.max(maxTimeInSystem, (int) (currentTime - arrivalTimes[numPartsProduced - 1]) + serviceTimes[numPartsProduced - 1]);
+        maxTimeInSystem = Math.max(maxTimeInSystem, (int) (currentTime - arrivalTimes[numPartsProduced - 1]) + (int)serviceTimes[numPartsProduced - 1]);
         areaUnderQLength += numPartsInQueue;
         highestQLength = Math.max(highestQLength, numPartsInQueue);
         areaUnderServerBusy += (numPartsInQueue > 0 ? 1 : 0) * (interarrivalTimes[numPartsProduced - 1] + serviceTimes[numPartsProduced - 1]);
@@ -87,32 +88,39 @@ public class RedoMidAct02 {
         statisticalAccumulators();
 
         String filePath = "res/data.csv";
-        int[] arrivalTimes = new int[11];
-        int[] interarrivalTimes = new int[11];
-        int[] serviceTimes = new int[11];
-
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             // Skip header row
             String header = br.readLine();
-            String line;
-            int index = 0;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                try {
-                    arrivalTimes[index] = (int) Double.parseDouble(values[1]);
-                    interarrivalTimes[index] = (int) Double.parseDouble(values[2]);
-                    serviceTimes[index] = (int) Double.parseDouble(values[3]);
-                } catch (NumberFormatException e) {
-                    System.err.printf("Error reading line %d of CSV file: invalid number format. Using default values.%n", index + 1);
-                    arrivalTimes[index] = 0;
-                    interarrivalTimes[index] = 0;
-                    serviceTimes[index] = 0;
+
+            // Read data
+            for (int i = 0; i < 11; i++) {
+                String line = br.readLine();
+                if (line == null) {
+                    throw new RuntimeException("Not enough data in input file");
                 }
-                index++;
+                String[] values = line.split(",");
+                if (values.length != 4) {
+                    throw new RuntimeException("Invalid data format in input file");
+                }
+                try {
+                    arrivalTimes[i] = (int) Double.parseDouble(values[1]);
+                    interarrivalTimes[i] = (int) Double.parseDouble(values[2]);
+                    serviceTimes[i] = (int) Double.parseDouble(values[3]);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException("Invalid data format in input file");
+                }
+            }
+
+            // Make sure there's no extra data in the input file
+            if (br.readLine() != null) {
+                throw new RuntimeException("Too much data in input file");
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Error reading input file: " + e.getMessage());
+            System.exit(1);
+        } catch (RuntimeException e) {
+            System.err.println("Error parsing input file: " + e.getMessage());
+            System.exit(1);
         }
     }
-
 }
